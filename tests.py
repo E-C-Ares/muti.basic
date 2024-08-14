44135b08ca32ee6f886ecfe5b9273edc4f9aaead
E.C.Ares <E.C.Ares@outlook.com>
2024-08-14 18:34:37 +0800

    0.1.0

---
+++ tests.py
@@ -0,0 +1,10 @@
+TFC_ARG =[['--f_abc',  False  , ['true']      ], 
+          ['--f_def',   True  , ['true']      ],
+          ['--f_dmm',  'cuda' , ['cuda','cpu']],  
+          ['--j_nym',  'here' ,    str        ],
+          ['--q_stp',10000    ,    int        ],
+          ['--z_gmm',    0.875,  float       ]]
+
+# TODO
+def getrArg( fc='n'):
+    return  TFC_ARG
